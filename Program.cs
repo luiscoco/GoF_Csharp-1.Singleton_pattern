ef7231053f118286e3bfdb5d25cd5c5ee60bbd7d
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-08-07 10:48:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Singleton
+{
+    private static Singleton instance;
+
+    // Private constructor to prevent external instantiation
+    private Singleton() { }
+
+    public static Singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public string Function() // Renamed the method to start with a capital letter
+    {
+        return "Hola";
+    }
+
+    // Add other methods and properties here
+}
+
+class Program
+{
+    static void Main()
+    {
+        Singleton singleton = Singleton.GetInstance();
+        Console.WriteLine(singleton.Function());
+    }
+}
